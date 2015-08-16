cf9b6f5d251be5e0b6d69c71ad2b9e10f1b37a47
Darren Fehrmann <darren.fehrmann@gmail.com>
2015-08-16 21:59:05 +0000

    Functional tests for POST

--- functional_tests.py
+++ functional_tests.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 from xvfbwrapper import Xvfb
 
-class NewVisitorTest(unittest.TestCase):
 
+class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -31,8 +31,22 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table -- its text was:\n%s" % (
+                table.text,
+            )
+        )
 
-        any(row.text == '1: Buy peacock feathers' for row in rows) )
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', [row.text for row in rows]
+        )
 
         self.fail('Finish the test!')
 
