c1bb491c71662454f1d66fa8d2f17b9befa37485
Darren Fehrmann <darren.fehrmann@gmail.com>
2015-08-16 21:27:35 +0000

    Use template on homepage

--- functional_tests.py
+++ functional_tests.py
@@ -1,26 +1,44 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
-
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-
-assert 'To-Do' in browser.title, "Browser title was " + browser.title
-
-browser.quit()
+from xvfbwrapper import Xvfb
 
 class NewVisitorTest(unittest.TestCase):
-	def setUp(self):
-		self.browser - webdriver.Firefox()
 
-	def tearDown(self):
-		self.browser.quit()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get('http://localhost:8080')
+    def tearDown(self):
+        self.browser.quit()
 
-		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+
+        any(row.text == '1: Buy peacock feathers' for row in rows) )
+
+        self.fail('Finish the test!')
+
 
 if __name__ == '__main__':
-	unittest.main(warnings='ignore')
-
+    vdisplay = Xvfb()
+    vdisplay.start()
+    unittest.main(warnings='ignore')
+    vdisplay.stop()
--- superlists/urls.py
+++ superlists/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
-from lists import views
 
 urlpatterns = [
-	url(r'^$', views.home_page, name='home'),
+	url(r'^$', 'lists.views.home_page', name='home'),
 ]
