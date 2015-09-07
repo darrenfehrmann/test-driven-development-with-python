d5cf790cbf057e5ce4ac8b74facf41b05dba09d3
Darren Fehrmann <darren.fehrmann@gmail.com>
2015-09-07 17:04:18 +0000

    Moved Fts into their own individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,28 +1,11 @@
+from .base import FunctionalTest
 from selenium import webdriver
-import os
 from selenium.webdriver.common.keys import Keys
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
-# Run with:
-# $ python3 manage.py test functional_tests
-class NewVisitorTest(StaticLiveServerTestCase):
-    def setUp(self):
-        # Set display for xvfb
-        os.environ["DISPLAY"] = ":99"
 
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
+class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -57,7 +40,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Francis visits the home page. There is no sign of Edith's
         # list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -76,20 +59,5 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertIn('Buy milk', page_text)
-
-    def test_layout_and_styling(self):
-        # Edith goes to the home page
-        self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
-
-        # She starts a new list and sees the input is nicely
-        # centered there too
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        # inputbox.send_keys('testing')
-        # inputbox.send_keys(Keys.ENTER)
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=5
-        )
+        self.assertIn('Buy milk', page_text)
